536e7034e7b976ba1c563de08c9eacad434aebe5
Tomas Padilla <padillatomasagustin@gmail.com>
2022-04-17 20:17:27 +0200

    Al 17/04 - Falta Backend

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import "./Register.sass";
+import { AiOutlineCloudUpload } from "react-icons/ai";
 
 import { useNavigate } from "react-router-dom";
 import authService from "../../../Services/auth.service";
 
 const Register = () => {
 	let navigate = useNavigate();
+	const [isProf, setIsProf] = useState(false);
+	const [fileName, setFileName] = useState(null);
 	const [data, setData] = useState({
 		user: {
 			email: "",
@@ -29,6 +32,17 @@ const Register = () => {
 		setData(newData);
 	};
 
+	const handleProf = () => {
+		setIsProf(!isProf);
+	};
+
+	// FileInput:
+	const handleFileName = (e) => {
+		const image = document.getElementById("image").files[0];
+		if (image.name) {
+			setFileName(image.name);
+		}
+	};
 	// onSubmit:
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -47,152 +61,170 @@ const Register = () => {
 		<section className="section page-sect">
 			<div className="section-center">
 				<div className="section-title">
-					<h2>Register</h2>
+					<h2>Registro</h2>
 				</div>
 			</div>
 			<div className="form-container">
+				<div className="isProfesional">
+					<input type="checkbox" name="isProf" onChange={handleProf} />
+					<label htmlFor="isProf">Eres Profesional?</label>
+					{isProf && <h2>Hola</h2>}
+				</div>
 				<form onSubmit={handleSubmit} className="register-form">
-					{/* Email */}
-					<div className="single-input">
-						<label htmlFor="username">Email</label>
-						<input
-							type="text"
-							name="username"
-							placeholder="E-mail"
-							id="email"
-							value={data.user.email}
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
+					{/* === CUENTA === */}
+					<div className="input-block">
+						{/* Email */}
+						<div className="single-input">
+							<label htmlFor="username">Email</label>
+							<input
+								type="text"
+								name="username"
+								placeholder="E-mail"
+								id="email"
+								value={data.user.email}
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* Password */}
+						<div className="single-input">
+							<label htmlFor="password">Contraseña</label>
+							<input
+								type="text"
+								name="password"
+								placeholder="Ingrese su contraseña"
+								value={data.user.password}
+								id="password"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
 					</div>
-					{/* Password */}
-					<div className="single-input">
-						<label htmlFor="password">Contraseña</label>
-						<input
-							type="text"
-							name="password"
-							placeholder="Ingrese su contraseña"
-							value={data.user.password}
-							id="password"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
+					{/* === PERSONA === */}
+					<div className="input-block">
+						{/* FOTO */}
+						<div>
+							<label htmlFor="image" className="file-input" id="myLabel">
+								<input
+									type="file"
+									name="image"
+									className="custom-file-input"
+									id="image"
+									onChange={(e) => {
+										handleFileName(e);
+										handleData(e);
+									}}
+								/>
+								{fileName ? <span>{fileName}</span> : "Subir Foto"}
+								<AiOutlineCloudUpload id="foto-icon"></AiOutlineCloudUpload>{" "}
+							</label>
+						</div>
+						{/* First_Name */}
+						<div className="single-input">
+							<label htmlFor="firstName">Nombre</label>
+							<input
+								type="text"
+								name="first_name"
+								placeholder="Ej: Cosme"
+								value={data.user.first_name}
+								id="first_name"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* Last_Name */}
+						<div className="single-input">
+							<label htmlFor="last_name">Apellido</label>
+							<input
+								type="text"
+								name="last_name"
+								placeholder="Ej: Fulanito"
+								value={data.user.last_name}
+								id="last_name"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* DNI */}
+						<div className="single-input">
+							<label htmlFor="dni">DNI</label>
+							<input
+								type="text"
+								name="dni"
+								placeholder="Ej: 35000000"
+								value={data.user.dni}
+								id="dni"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* Age */}
+						<div className="single-input">
+							<label htmlFor="dni">Edad</label>
+							<input
+								type="number"
+								name="age"
+								placeholder="Ej: 38"
+								value={data.user.age}
+								id="age"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
 					</div>
-					{/* FOTO */}
-					<div className="file-input">
-						<label htmlFor="password">Foto De Perfil</label>
-						<input
-							type="file"
-							name="image"
-							placeholder="Ingrese su contraseña"
-							id="image"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* First_Name */}
-					<div className="single-input">
-						<label htmlFor="firstName">Nombre</label>
-						<input
-							type="text"
-							name="first_name"
-							placeholder="Ej: Cosme"
-							value={data.user.first_name}
-							id="first_name"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* Last_Name */}
-					<div className="single-input">
-						<label htmlFor="last_name">Apellido</label>
-						<input
-							type="text"
-							name="last_name"
-							placeholder="Ej: Fulanito"
-							value={data.user.last_name}
-							id="last_name"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* DNI */}
-					<div className="single-input">
-						<label htmlFor="dni">DNI</label>
-						<input
-							type="text"
-							name="dni"
-							placeholder="Ej: 35000000"
-							value={data.user.dni}
-							id="dni"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* Age */}
-					<div className="single-input">
-						<label htmlFor="dni">Edad</label>
-						<input
-							type="number"
-							name="age"
-							placeholder="Ej: 38"
-							value={data.user.age}
-							id="age"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* Country */}
-					<div className="single-input">
-						<label htmlFor="country">Pais</label>
-						<input
-							type="text"
-							name="country"
-							placeholder="Ej: Argentina"
-							value={data.user.country}
-							id="country"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* Province */}
-					<div className="single-input">
-						<label htmlFor="province">Provincia</label>
-						<input
-							type="text"
-							name="province"
-							placeholder="Ej: Buenos Aires"
-							value={data.user.province}
-							id="province"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
-					</div>
-					{/* City */}
-					<div className="single-input">
-						<label htmlFor="city">Ciudad</label>
-						<input
-							type="text"
-							name="city"
-							placeholder="Ej: Olivos"
-							value={data.user.city}
-							id="city"
-							onChange={(e) => {
-								handleData(e);
-							}}
-						/>
+					{/* === COORDONEES === */}
+					<div className="input-block">
+						{/* Country */}
+						<div className="single-input">
+							<label htmlFor="country">Pais</label>
+							<input
+								type="text"
+								name="country"
+								placeholder="Ej: Argentina"
+								value={data.user.country}
+								id="country"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* Province */}
+						<div className="single-input">
+							<label htmlFor="province">Provincia</label>
+							<input
+								type="text"
+								name="province"
+								placeholder="Ej: Buenos Aires"
+								value={data.user.province}
+								id="province"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
+						{/* City */}
+						<div className="single-input">
+							<label htmlFor="city">Ciudad</label>
+							<input
+								type="text"
+								name="city"
+								placeholder="Ej: Olivos"
+								value={data.user.city}
+								id="city"
+								onChange={(e) => {
+									handleData(e);
+								}}
+							/>
+						</div>
 					</div>
 					<button type="submit" className="btn-pastel">
-						Registro
+						Enviar
 					</button>
 				</form>
 			</div>
--- src/Pages/Plataform/Dashboard/Dashboard.jsx
+++ src/Pages/Plataform/Dashboard/Dashboard.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.sass";
 
+import profilePlaceholder from "../../../Assets/profilePlaceholder.jpeg";
+
+import Loading from "../../../Components/Loading/Loading";
+
 import authService from "../../../Services/auth.service";
 import userService from "../../../Services/user.service";
-import Loading from "../../../Components/Loading/Loading";
 
 const Dashboard = () => {
 	let navigate = useNavigate();
-	const [email, setEmail] = useState(null);
 	const [role, setRole] = useState(null);
 	const [meData, setMeData] = useState(null);
 
@@ -16,6 +18,17 @@ const Dashboard = () => {
 		userService.getMeData().then(
 			(response) => {
 				setMeData(response.data);
+				response.data.roles.map((singleRole) => {
+					if (singleRole.name === "ROLE_PATIENT") {
+						setRole("patient");
+					}
+					if (singleRole.name === "ROLE_PROFESSIONAL") {
+						setRole("professional");
+					}
+					if (singleRole.name === "ROLE_ADMIN") {
+						setRole("admin");
+					}
+				});
 			},
 			(error) => {
 				console.log(error);
@@ -28,26 +41,175 @@ const Dashboard = () => {
 		navigate("/");
 		window.location.reload();
 	};
-	if (meData) {
+
+	// Methods:
+	const fullName = (first, last) => {
+		return first + " " + last;
+	};
+	const fullVivienda = (prov, city) => {
+		return prov + ", " + city;
+	};
+
+	// === PATIENT ===
+	if (role === "patient") {
+		const {
+			username,
+			email,
+			image_url,
+			firt_name,
+			last_name,
+			dni,
+			age,
+			country,
+			province,
+			city,
+			id,
+		} = meData;
 		return (
 			<section className="section page-sect">
 				<div className="section-center">
 					<div className="section-title">
-						<span>{meData.username}</span>
-						<h2>Main Dashboard </h2>
+						<span>{fullName(firt_name, last_name)}</span>
+						<h2>Mi Perfil </h2>
 					</div>
-					<h2>{meData.email}</h2>
-					<h2>{meData.firt_name}</h2>
-					<h2>{meData.last_name}</h2>
-					<h2>{meData.country}</h2>
-					<h2>{meData.province}</h2>
-					<h2>{meData.city}</h2>
-					<h2>{meData.dni}</h2>
-					<h2>{meData.age}</h2>
+					<div className="main-container">
+						{/* Img, Name */}
+						<hr />
+						<div className="data-container">
+							<img src={image_url || profilePlaceholder} alt="Imagen Perfil" />
+							<div className="data">
+								<h2>{fullName(firt_name, last_name)}</h2>
+								<h3>{fullVivienda(province, country)}</h3>
+							</div>
+						</div>
+						<hr />
+						{/* Profesionales */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Mis Profesionales</h2>
+						</article>
+						{/* Facturacion */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Mi Facturación</h2>
+						</article>
+						{/* Tarjetas */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Mis Tarjetas</h2>
+						</article>
+						{/* Calendar */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Calendario</h2>
+						</article>
+						<button
+							type="submit"
+							onClick={() => {
+								handleLogout();
+							}}
+							className="btn-pastel"
+						>
+							Logout
+						</button>
+					</div>
 				</div>
-				<button type="submit" onClick={handleLogout}>
-					Logout
-				</button>
+			</section>
+		);
+	}
+
+	// === PROFESSIONAL ===
+	if (role === "professional") {
+		const {
+			username,
+			email,
+			image_url,
+			firt_name,
+			last_name,
+			dni,
+			age,
+			country,
+			province,
+			city,
+			id,
+		} = meData;
+		return (
+			<section className="section page-sect">
+				<div className="section-center">
+					<div className="section-title">
+						<span>{fullName(firt_name, last_name)}</span>
+						<h2>Mi Perfil </h2>
+					</div>
+					<div className="main-container">
+						{/* Img, Name */}
+						<hr />
+						<div className="data-container">
+							<img src={image_url || profilePlaceholder} alt="Imagen Perfil" />
+							<div className="data">
+								<h2>{fullName(firt_name, last_name)}</h2>
+								<h3>{fullVivienda(province, country)}</h3>
+							</div>
+						</div>
+						<hr />
+						{/* Turnos */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Proximos Turnos</h2>
+						</article>
+						{/* Calendario */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Calendario</h2>
+						</article>
+						<button type="submit" onClick={handleLogout} className="btn-pastel">
+							Logout
+						</button>
+					</div>
+				</div>
+			</section>
+		);
+	}
+
+	// === ADMIN ===
+	if (role === "admin") {
+		const {
+			username,
+			email,
+			image_url,
+			firt_name,
+			last_name,
+			dni,
+			age,
+			country,
+			province,
+			city,
+			id,
+		} = meData;
+		return (
+			<section className="section page-sect">
+				<div className="section-center">
+					<div className="section-title">
+						<span>{fullName(firt_name, last_name)}</span>
+						<h2>Mi Perfil </h2>
+					</div>
+					<div className="main-container">
+						{/* Img, Name */}
+						<hr />
+						<div className="data-container">
+							<img src={image_url || profilePlaceholder} alt="Imagen Perfil" />
+							<div className="data">
+								<h2>{fullName(firt_name, last_name)}</h2>
+								<h3>{fullVivienda(province, country)}</h3>
+							</div>
+						</div>
+						<hr />
+						{/* Usuarios */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Lista Usuarios</h2>
+						</article>
+						{/* Profesionales */}
+						<article className="single-block">
+							<h2 className="section-title subtitle">Lista Profesionales</h2>
+						</article>
+						<button type="submit" onClick={handleLogout} className="btn-pastel">
+							Logout
+						</button>
+					</div>
+				</div>
 			</section>
 		);
 	} else {
